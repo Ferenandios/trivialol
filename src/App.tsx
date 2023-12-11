d10a0c5619f9cbe0b21d620878f57686a7a88420
Ferenandios <www.terunplay@gmail.com>
2023-12-11 09:36:00 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const App: FC = (): JSX.Element => {
+  return (
+    <>
+      <div className="bg-lime-200">it's just works!</div>
+    </>
+  );
+};
+
+export default App;
