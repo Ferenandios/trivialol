8ae4c7b6829c5cfc8bc913ae981c415826ccfac1
Ferenandios <www.terunplay@gmail.com>
2023-12-13 09:45:05 +0300

    Created Store for Plan-component

---
+++ src/store/store.ts
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import personSlice from "../features/person.slice";
+
+export const store = configureStore({ reducer: { person: personSlice } });
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
