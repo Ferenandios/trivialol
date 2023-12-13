77027a27cdd066dfba142586fd32192794139d71
Ferenandios <www.terunplay@gmail.com>
2023-12-13 09:42:34 +0300

    Created InvestButton-component

---
+++ src/components/SteveJobs/SteveJobs.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+
+const SteveJobs: FC = (): JSX.Element => {
+  return (
+    <>
+      <div className="px-4 mt-4">
+        <div className="w-full h-[60px] bg-[#D9D9D91A] rounded-xl font-raleway font-black text-lg flex justify-center items-center  ">
+          <a
+            href="https://www.youtube.com/watch?v=3nJs6GPmEZs"
+            target="_blank"
+            className="w-full h-full flex justify-center items-center"
+          >
+            Поддержать продукт
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SteveJobs;
