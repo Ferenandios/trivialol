389efde2d89a24ea13e71d3842a1ef006a7826b1
Ferenandios <www.terunplay@gmail.com>
2023-12-13 09:43:13 +0300

    Added two components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Banner from "./components/Banner/Banner";
 import About from "./components/About/About";
 import Team from "./components/Team/Team";
 import Footer from "./components/Footer/Footer";
+import Plan from "./components/Plan/Plan";
+import SteveJobs from "./components/SteveJobs/SteveJobs";
 
 const App: FC = (): JSX.Element => {
   return (
@@ -12,6 +14,8 @@ const App: FC = (): JSX.Element => {
           <Banner />
           <About />
           <Team />
+          <Plan />
+          <SteveJobs />
           <Footer />
         </main>
       </div>
