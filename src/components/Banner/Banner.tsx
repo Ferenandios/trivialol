43a2a61ddd6831eb1dfd24efc94eda2b64f8d7ff
Ferenandios <www.terunplay@gmail.com>
2023-12-23 15:47:21 +0300

    Changed background

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -2,13 +2,20 @@ import { FC } from "react";
 import Title from "./Title/Title";
 import Subtitle from "./Subtitle/Subtitle";
 import styles from "./Banner.module.css";
-import background from "/src/assets/banner.png";
+import background from "/src/assets/banner.webp";
 
 const Banner: FC = (): JSX.Element => {
   return (
     <>
       <div className={styles.banner}>
-        <img className={styles.background} src={background} alt="Background" />
+        <div className={styles.background}>
+          <div className="absolute -z-40 w-full h-full bg-[#111] bg-opacity-70 bg-gradient-to-b from-transparent to-[#111] to-[100%]"></div>
+          <img
+            className="absolute -z-50 w-full h-full object-cover grayscale left-0"
+            src={background}
+            alt="Background"
+          />
+        </div>
         <div className="container">
           <div className={styles.inner}>
             <Title />
