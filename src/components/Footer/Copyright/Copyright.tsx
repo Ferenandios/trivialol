d624d197a9910c222d8c3500909981a0f86936d8
Ferenandios <www.terunplay@gmail.com>
2023-12-23 15:48:29 +0300

    Updated from text to link

--- src/components/Footer/Copyright/Copyright.tsx
+++ src/components/Footer/Copyright/Copyright.tsx
@@ -1,12 +1,17 @@
 import { FC } from "react";
-import styles from './Copyright.module.css'
+import styles from "./Copyright.module.css";
 
 const Copyright: FC = (): JSX.Element => {
   return (
     <>
       <div className={styles.ansoc}>
         <h6>«Невероятно простой»</h6>
-        <h6 className={styles.copyright}>&copy; 2023, Рофлан </h6>
+        <h6 className={styles.copyright}>
+          &copy; 2023,{" "}
+          <a className="underline" href="https://github.com/Ferenandios">
+            Князев Николай
+          </a>{" "}
+        </h6>
       </div>
     </>
   );
