4b75d3c7e6ab3538b09ef62dd8f94e9984b7053e
Ferenandios <www.terunplay@gmail.com>
2023-12-23 15:49:32 +0300

    Added new element for copied Link

--- src/components/Footer/Links/Links.tsx
+++ src/components/Footer/Links/Links.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
 import { type ILinks } from "../../../types/types";
 import Link from "./Link/Link";
-import styles from './Links.module.css'
+import styles from "./Links.module.css";
+import { FaCircleCheck } from "react-icons/fa6";
+import { useAppSelector } from "../../../hooks/hooks";
 
 const Links: FC = (): JSX.Element => {
+  const { showCopy } = useAppSelector((state) => state.person);
   const links: ILinks[] = [
     { link: "https://t.me/kolyanv", social: "Telegram" },
     { link: "mailto:www.terunplay@gmail.com", social: "Email" },
@@ -13,9 +16,19 @@ const Links: FC = (): JSX.Element => {
     <>
       <div className={styles.links}>
         {links.map((link, index) => (
-          <Link key={index} link={link} />
+          <Link key={index} link={link} id={index} />
         ))}
       </div>
+      {showCopy && (
+        <div className="fixed z-50 flex justify-center w-full top-10">
+          <div className="bg-[#D9D9D91A] rounded-md px-3 py-2">
+            <span className="flex items-center">
+              <FaCircleCheck size={15} className="mr-1" />
+              Скопирована!
+            </span>
+          </div>
+        </div>
+      )}
     </>
   );
 };
