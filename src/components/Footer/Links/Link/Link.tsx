15519c26638ae7a5b7d6b59faf74b229ba0fcff3
Ferenandios <www.terunplay@gmail.com>
2023-12-23 15:50:30 +0300

    Added useClipboard for buttons

--- src/components/Footer/Links/Link/Link.tsx
+++ src/components/Footer/Links/Link/Link.tsx
@@ -1,13 +1,34 @@
 import { FC } from "react";
 import { type LinkProps } from "../../../../types/types";
 import styles from "./Link.module.css";
+import { useClipboard } from "use-clipboard-copy";
+import { useAppDispatch } from "../../../../hooks/hooks";
+import { displayCopyWindow } from "../../../../features/person.slice";
 
-const Link: FC<LinkProps> = ({ link }): JSX.Element => {
+const Link: FC<LinkProps> = ({ link, id }): JSX.Element => {
+  const clipboard = useClipboard();
+  const dispatch = useAppDispatch();
   return (
     <>
-      <a className={styles.link} target="_blank" href={link.link}>
-        {link.social}
-      </a>
+      {id !== 2 ? (
+        <button
+          onClick={() => {
+            clipboard.copy(link.link);
+            dispatch(displayCopyWindow());
+          }}
+          className={styles.link}
+        >
+          {link.social}
+        </button>
+      ) : (
+        <a
+          href="https://habr.com/ru/users/Ferenando"
+          target="_blank"
+          className={styles.link}
+        >
+          {link.social}
+        </a>
+      )}
     </>
   );
 };
